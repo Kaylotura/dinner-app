1ba313e4b4f7998fd331792a95228a8de828ecaf
Eric Chase <echase6@gmail.com>
2016-08-19 11:58:25 -0700

    adding food-to-wine mapping

---
+++ wine.js
@@ -0,0 +1,15 @@
+'use strict';
+
+WINE_LIST = {
+  VEGETABLES: ['Dry White', 'Sparkling'],
+  ROASTED_VEG: ['Dry White', 'Light Red', 'Medium Red'],
+  SOFT_CHEESE: ['Sweet White', 'Rich White', 'Sparkling', 'Dessert'],
+  HARD_CHEESE: ['Sweet White', 'Sparkling', 'Medium Red', 'Bold Red'],
+  STARCHES: ['Dry White', 'Rich White', 'Sparkling', 'Light Red', 'Medium Red', 'Bold Red', 'Dessert'],
+  FISH: ['Dry White', 'Rich White', 'Sparkling'],
+  RICH_FISH: ['Light Red', 'Rich White'],
+  WHITE_MEAT: ['Rich White', 'Light Red', 'Medium Red'],
+  RED_MEAT: ['Medium Red', 'Bold Red'],
+  CURED_MEAT: ['Sweet White', 'Light Red', 'Medium Red', 'Bold Red', 'Dessert'],
+  SWEETS: ['Sweet White', 'Dessert']
+}
