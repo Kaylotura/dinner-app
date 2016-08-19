04eb7cecfa8393f16884c8ce71a58c2449f81e9a
Katie Nichols <Kaylotura@gmail.com>
2016-08-19 15:48:13 -0700

    honing request

--- site.js
+++ site.js
@@ -4,7 +4,7 @@
  * Takes in a single hit and returns the title of the recipe as a string.
  */
 function formatDish(dishObject) {
-  return dishObject.recipe.label;
+  return dishObject['recipe']['label'];
 }
 
 
@@ -12,19 +12,20 @@ function formatDish(dishObject) {
  * Takes in the hits returned by the Edamam and returns a list of strings.
  */
 function formatDishes(dishes) {
-  return _.map(dishes.hits.hit, formatDish);
+  console.dir(dishes);
+  return _.map(dishes['hits']['hit'], formatDish);
 }
 
 /**
  * Runs a search request with Edemam using the given string 'chicken', and if
  * successful, it runs the json object through the formatDishes function.
  */
+
 $.ajax({
   url: 'https://api.edamam.com/search',
   data: {q: 'chicken' , app_id: edemamID , app_key: edemamKey},
-  success: formatDish(),
+  success: formatDishes(),
   error: function(error) {
     alert(error);
   }
 });
-
