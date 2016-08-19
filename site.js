14e5bbdefd2f72ccb4bf16e9354aae624a47e2b8
Katie Nichols <Kaylotura@gmail.com>
2016-08-19 15:39:32 -0700

    More complete site.js

--- site.js
+++ site.js
@@ -1,31 +1,10 @@
 'use strict';
 
 /**
- * Takes in a comma-seperated string of ingredients and returns a promise
- * to return a json object of dish options information.
- *
- * Having some technical difficulties with Edamam Api, specifically that we are
- * limited to 50 free requests before they begin charging us .7 cents per
- * request, and that I do not have an API key yet? In the interum I will be working
- * on formating the results for two example requests.
- */
-function getRecipies(ingredients) {
-  PARAMS = {
-    ingredients: getIngredients()
-  }
-  var url = 'https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients';
-  return Promise.resolve($.ajax({
-    parameters: PARAMS,
-    dataType: 'json',
-    url: url,
-  }));
-}
-
-/**
  * Takes in a single hit and returns the title of the recipe as a string.
  */
 function formatDish(dishObject) {
-  return dishObject.recipe.lable;
+  return dishObject.recipe.label;
 }
 
 
@@ -37,17 +16,15 @@ function formatDishes(dishes) {
 }
 
 /**
- *
- *
+ * Runs a search request with Edemam using the given string 'chicken', and if
+ * successful, it runs the json object through the formatDishes function.
  */
-
-
 $.ajax({
   url: 'https://api.edamam.com/search',
-  data: {q: 'chicken' , app_id: 565656 , app_key: 909090},
+  data: {q: 'chicken' , app_id: edemamID , app_key: edemamKey},
   success: formatDish(),
   error: function(error) {
-    alert(error)
+    alert(error);
   }
+});
 
-})
